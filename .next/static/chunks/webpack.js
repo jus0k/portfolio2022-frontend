4c37e420719bd15aaed2b20ded263dd4b6f5f421
jus0k <jutjd115@gmail.com>
2021-12-12 16:25:04 +0900

    fix: babelrc 린트 설정 수정

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "55b244199cff9a03e73f"; }
+/******/ 		__webpack_require__.h = function() { return "cbc3823249dc542507bb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
